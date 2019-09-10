a79284b37ebd715ffc6bd92f9614749d7aa941c9
Vitry François <vitryfrancois@pcparadis-102.cirad.fr>
2019-09-10 13:01:39 +0400

    Comment test

--- geonode/utils.py
+++ geonode/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #########################################################################
-#
+# TSET FV
 # Copyright (C) 2016 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
